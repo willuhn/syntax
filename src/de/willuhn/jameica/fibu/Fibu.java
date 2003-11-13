cc0aaafc03583d9f5ac7a98ef14c1e34694e23e4
willuhn <willuhn>
2003-11-13 00:36:12 +0000

    *** empty log message ***

---
+++ src/de/willuhn/jameica/fibu/Fibu.java
@@ -0,0 +1,47 @@
+/**********************************************************************
+ * $Source: /cvsroot/syntax/syntax/src/de/willuhn/jameica/fibu/Fibu.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2003/11/13 00:36:12 $
+ * $Author: willuhn $
+ * $Locker:  $
+ * $State: Exp $
+ *
+ * Copyright (c) by willuhn.webdesign
+ * All rights reserved
+ *
+ **********************************************************************/
+package de.willuhn.jameica.fibu;
+
+import de.willuhn.jameica.Plugin;
+
+/**
+ * Basisklasse des Fibu-Plugins fuer das Jameica-Framework.
+ * @author willuhn
+ */
+public class Fibu extends Plugin
+{
+
+  /**
+   * Initialisiert das Plugin.
+   * @see de.willuhn.jameica.Plugin#init()
+   */
+  public void init()
+  {
+  }
+
+  /**
+   * Beendet das Plugin.
+   * @see de.willuhn.jameica.Plugin#shutDown()
+   */
+  public void shutDown()
+  {
+  }
+
+}
+
+/*********************************************************************
+ * $Log: Fibu.java,v $
+ * Revision 1.1  2003/11/13 00:36:12  willuhn
+ * *** empty log message ***
+ *
+ **********************************************************************/
