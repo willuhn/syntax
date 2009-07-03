6eba564d3593f11a3990bde3bbf64f1dd40df087
willuhn <willuhn>
2009-07-03 11:12:09 +0000

    @B 1.7 compatibility

--- src/de/willuhn/jameica/fibu/ext/JameicaCompatibility.java
+++ src/de/willuhn/jameica/fibu/ext/JameicaCompatibility.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/syntax/syntax/src/de/willuhn/jameica/fibu/ext/Attic/JameicaCompatibility.java,v $
- * $Revision: 1.2 $
- * $Date: 2009/07/03 10:52:19 $
+ * $Revision: 1.3 $
+ * $Date: 2009/07/03 11:12:09 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -13,13 +13,15 @@
 
 package de.willuhn.jameica.fibu.ext;
 
+import de.willuhn.datasource.BeanUtil;
 import de.willuhn.jameica.fibu.gui.part.BuchungList;
 import de.willuhn.jameica.fibu.gui.part.KontoList;
 import de.willuhn.jameica.gui.extension.Extendable;
 import de.willuhn.jameica.gui.extension.Extension;
 import de.willuhn.jameica.gui.parts.TablePart;
-import de.willuhn.jameica.plugin.Version;
+import de.willuhn.jameica.plugin.Manifest;
 import de.willuhn.jameica.system.Application;
+import de.willuhn.logging.Logger;
 
 /**
  * Diese Klasse ruestet Funktionen nach, die erst in Jameica 1.8 verfuegbar sind.
@@ -34,16 +36,28 @@ public class JameicaCompatibility implements Extension
    */
   public void extend(Extendable extendable)
   {
-    Version v = Application.getManifest().getVersion();
-    if (v.getMajor() < 1) return; // geht mal gar nicht ;)
-    if (v.getMinor() < 8) return; // Hier wird 1.7 ausgefiltert
+    Manifest mf = Application.getManifest();
     
-    String id = extendable.getExtendableID();
-    if (KontoList.class.getName().equals(id) ||
-        BuchungList.class.getName().equals(id))
+    // Wir koennen die Funktion getVersion() nicht direkt
+    // aufrufen, weil sie beim Compilieren ggf. gegen die
+    // falsche Version gelinkt ist.
+    try
     {
-      TablePart t = (TablePart) extendable;
-      t.setRememberState(true);
+      Object version = BeanUtil.get(mf,"version");
+      if (version instanceof Double)
+        return; // <= 1.7 - da gibt es "TablePart#setRememberState" noch nicht
+      
+      String id = extendable.getExtendableID();
+      if (KontoList.class.getName().equals(id) ||
+          BuchungList.class.getName().equals(id))
+      {
+        TablePart t = (TablePart) extendable;
+        t.setRememberState(true);
+      }
+    }
+    catch (Throwable t)
+    {
+      Logger.error("unable to activate feature",t);
     }
   }
 
@@ -52,6 +66,9 @@ public class JameicaCompatibility implements Extension
 
 /**********************************************************************
  * $Log: JameicaCompatibility.java,v $
+ * Revision 1.3  2009/07/03 11:12:09  willuhn
+ * @B 1.7 compatibility
+ *
  * Revision 1.2  2009/07/03 10:52:19  willuhn
  * @N Merged SYNTAX_1_3_BRANCH into HEAD
  *
