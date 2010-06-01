517ff378d8ed001f00b6d17b8c1f93a458c856f5
willuhn <willuhn>
2010-06-01 16:35:48 +0000

    @C Konstanten verschoben

--- src/de/willuhn/jameica/fibu/Fibu.java
+++ src/de/willuhn/jameica/fibu/Fibu.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/syntax/syntax/src/de/willuhn/jameica/fibu/Fibu.java,v $
- * $Revision: 1.46 $
- * $Date: 2009/07/03 10:52:19 $
+ * $Revision: 1.47 $
+ * $Date: 2010/06/01 16:35:48 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -12,13 +12,7 @@
  **********************************************************************/
 package de.willuhn.jameica.fibu;
 
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 
-import de.willuhn.jameica.fibu.gui.util.CustomDateFormat;
-import de.willuhn.jameica.fibu.gui.util.CustomDecimalFormat;
-import de.willuhn.jameica.fibu.update.Update;
 import de.willuhn.jameica.gui.MenuItem;
 import de.willuhn.jameica.gui.NavigationItem;
 import de.willuhn.jameica.gui.extension.Extendable;
@@ -27,11 +21,9 @@ import de.willuhn.jameica.gui.extension.ExtensionRegistry;
 import de.willuhn.jameica.gui.internal.views.Start;
 import de.willuhn.jameica.plugin.AbstractPlugin;
 import de.willuhn.jameica.plugin.Manifest;
-import de.willuhn.jameica.plugin.Version;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
-import de.willuhn.util.ClassFinder;
 
 /**
  * Basisklasse des Fibu-Plugins fuer das Jameica-Framework.
@@ -39,57 +31,6 @@ import de.willuhn.util.ClassFinder;
  */
 public class Fibu extends AbstractPlugin
 {
-
-  /**
-   * Dateformatter.
-   */
-  public final static DateFormat LONGDATEFORMAT   = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-
-  /**
-   * Dateformatter.
-   */
-  public final static DateFormat DATEFORMAT       = new SimpleDateFormat("dd.MM.yyyy");
-  
-  /**
-   * Dateformatter fuer Kurz-Format.
-   */
-  public final static DateFormat FASTDATEFORMAT   = new SimpleDateFormat("ddMMyyyy");
-  
-  /**
-   * Dateformatter fuer Buchungen.
-   */
-  public final static DateFormat BUCHUNGDATEFORMAT   = new SimpleDateFormat("ddMMyy");
-  
-  /**
-   * Unser eigenes kombiniertes Dateformat.
-   */
-  public final static DateFormat CUSTOM_DATEFORMAT   = new CustomDateFormat();
-
-  /**
-   * Wochentage
-   */
-  public final static String[] WEEKDAYS = new String[] {
-    "Sonntag",
-    "Montag",
-    "Dienstag",
-    "Mittwoch",
-    "Donnerstag",
-    "Freitag",
-    "Sonnabend"
-  };
-
-  /**
-   * DecimalFormat.
-   */
-  public final static DecimalFormat DECIMALFORMAT = new CustomDecimalFormat();
-
-  /**
-   */
-  public Fibu()
-  {
-    super();
-  }
-
 
   /**
    * @see de.willuhn.jameica.plugin.AbstractPlugin#init()
@@ -134,78 +75,13 @@ public class Fibu extends AbstractPlugin
       }, Start.class.getName());
     }
   }
-
-  /**
-   * @see de.willuhn.jameica.plugin.AbstractPlugin#shutDown()
-   */
-  public void shutDown()
-  {
-  }
-
-  /**
-   * @see de.willuhn.jameica.plugin.AbstractPlugin#install()
-   */
-  public void install() throws ApplicationException
-  {
-  }
-  
-  /**
-   * @see de.willuhn.jameica.plugin.AbstractPlugin#update(de.willuhn.jameica.plugin.Version)
-   */
-  public void update(Version oldVersion) throws ApplicationException
-  {
-    if (Application.inClientMode())
-      return; // Kein Update im Client-Mode oder beim ersten Start noetig.
-
-    Settings.setInUpdate(true);
-    Version newVersion = getManifest().getVersion();
-
-    Logger.info("starting update process for syntax [" + oldVersion + " -> " + newVersion + "]");
-    
-    ClassFinder finder = Application.getClassLoader().getClassFinder();
-    
-    try
-    {
-      Class[] updates = finder.findImplementors(Update.class);
-      if (updates == null || updates.length == 0)
-      {
-        Logger.info("no updates found");
-        return;
-      }
-      Logger.info("found " + updates.length + " update(s)");
-      for (int i=0;i<updates.length;++i)
-      {
-        Logger.info("applying update " + updates[i].getName());
-        Update update = (Update) updates[i].newInstance();
-        double ov = Double.parseDouble(oldVersion.getMajor() + "." + oldVersion.getMinor());
-        double nv = Double.parseDouble(newVersion.getMajor() + "." + newVersion.getMinor());
-        update.update(Application.getCallback().getStartupMonitor(),ov,nv);
-        Logger.info("update applied");
-      }
-      Logger.info("all updates applied");
-    }
-    catch (ClassNotFoundException cne)
-    {
-      Logger.info("no updates found");
-      return;
-    }
-    catch (ApplicationException ae)
-    {
-      throw ae;
-    }
-    catch (Exception e)
-    {
-      throw new ApplicationException(getResources().getI18N().tr("Fehler beim Update der Datenbank"),e);
-    }
-    finally
-    {
-      Settings.setInUpdate(false);
-    }
-  }
 }
 
 /*********************************************************************
  * $Log: Fibu.java,v $
+ * Revision 1.47  2010/06/01 16:35:48  willuhn
+ * @C Konstanten verschoben
+ *
  * Revision 1.46  2009/07/03 10:52:19  willuhn
  * @N Merged SYNTAX_1_3_BRANCH into HEAD
  *
@@ -217,141 +93,4 @@ public class Fibu extends AbstractPlugin
  *
  * Revision 1.44  2008/02/07 23:08:39  willuhn
  * @R KontenrahmenUtil#move() entfernt - hoffnungsloses Unterfangen
- *
- * Revision 1.43  2007/03/06 15:22:36  willuhn
- * @C Anlagevermoegen in Auswertungen ignorieren, wenn Anfangsbestand bereits 0
- * @B Formatierungsfehler bei Betraegen ("-0,00")
- * @C Afa-Buchungen werden nun auch als GWG gebucht, wenn Betrag zwar groesser als GWG-Grenze aber Afa-Konto=GWG-Afa-Konto (laut Einstellungen)
- *
- * Revision 1.42  2007/02/27 18:50:59  willuhn
- * @B 12- statt 24h-Format
- *
- * Revision 1.41  2006/12/27 14:42:23  willuhn
- * @N Update fuer MwSt.-Erhoehung
- *
- * Revision 1.40  2006/11/17 00:11:20  willuhn
- * *** empty log message ***
- *
- * Revision 1.39  2006/10/10 22:30:07  willuhn
- * @C DialogInput gegen DateInput ersetzt
- *
- * Revision 1.38  2006/06/30 14:09:56  willuhn
- * @N merged new pluginloader into HEAD
- *
- * Revision 1.37  2006/06/29 23:09:28  willuhn
- * @C keine eigene Startseite mehr, jetzt alles ueber Jameica-Boxsystem geregelt
- *
- * Revision 1.36  2006/06/19 22:41:47  willuhn
- * *** empty log message ***
- *
- * Revision 1.35  2006/06/19 16:25:42  willuhn
- * *** empty log message ***
- *
- * Revision 1.34  2006/06/12 15:41:18  willuhn
- * *** empty log message ***
- *
- * Revision 1.33  2006/06/12 14:08:30  willuhn
- * @N DB-Wizard
- *
- * Revision 1.32  2006/05/30 23:22:55  willuhn
- * @C Redsign beim Laden der Buchungen. Jahresabschluss nun korrekt
- *
- * Revision 1.31  2006/05/29 23:05:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.30  2006/05/08 15:41:57  willuhn
- * @N Buchungen als geprueft/ungeprueft markieren
- * @N Link Anlagevermoegen -> Buchung
- *
- * Revision 1.29  2006/01/03 17:55:53  willuhn
- * @N a lot more checks
- * @B NPEs
- * @N BuchungsTemplates pro Mandant/Kontenrahmen
- * @N Default-Geschaeftsjahr in init.sql verschoben
- * @N Handling von Eingabe von Altbestaenden im AV
- *
- * Revision 1.28  2005/09/26 15:15:39  willuhn
- * *** empty log message ***
- *
- * Revision 1.27  2005/09/24 13:00:13  willuhn
- * @B bugfixes according to bugzilla
- *
- * Revision 1.26  2005/08/25 23:00:02  willuhn
- * *** empty log message ***
- *
- * Revision 1.25  2005/08/25 21:58:58  willuhn
- * @N SKR04
- *
- * Revision 1.24  2005/08/22 16:37:22  willuhn
- * @N Anfangsbestaende
- *
- * Revision 1.23  2005/08/16 17:39:24  willuhn
- * *** empty log message ***
- *
- * Revision 1.22  2005/08/15 23:38:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.21  2005/08/12 00:10:59  willuhn
- * @B bugfixing
- *
- * Revision 1.20  2005/08/10 17:48:02  willuhn
- * @C refactoring
- *
- * Revision 1.19  2005/08/09 23:53:34  willuhn
- * @N massive refactoring
- *
- * Revision 1.18  2005/08/08 22:54:16  willuhn
- * @N massive refactoring
- *
- * Revision 1.17  2005/08/08 21:35:46  willuhn
- * @N massive refactoring
- *
- * Revision 1.16  2004/02/09 13:05:13  willuhn
- * @C misc
- *
- * Revision 1.15  2004/01/29 00:31:33  willuhn
- * *** empty log message ***
- *
- * Revision 1.14  2004/01/29 00:06:47  willuhn
- * *** empty log message ***
- *
- * Revision 1.13  2004/01/27 22:47:30  willuhn
- * *** empty log message ***
- *
- * Revision 1.12  2004/01/27 00:09:10  willuhn
- * *** empty log message ***
- *
- * Revision 1.11  2004/01/25 19:44:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.10  2004/01/03 18:07:22  willuhn
- * @N Exception logging
- *
- * Revision 1.9  2003/12/11 21:00:35  willuhn
- * @C refactoring
- *
- * Revision 1.8  2003/12/01 21:23:00  willuhn
- * *** empty log message ***
- *
- * Revision 1.7  2003/11/27 00:21:05  willuhn
- * @N Checks via insertCheck(), deleteCheck() updateCheck() in Business-Logik verlagert
- *
- * Revision 1.6  2003/11/25 01:23:19  willuhn
- * @N added Menu shortcuts
- *
- * Revision 1.5  2003/11/24 23:02:11  willuhn
- * @N added settings
- *
- * Revision 1.4  2003/11/24 14:21:56  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2003/11/20 03:48:44  willuhn
- * @N first dialogues
- *
- * Revision 1.2  2003/11/14 00:49:28  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2003/11/13 00:36:12  willuhn
- * *** empty log message ***
- *
  **********************************************************************/
--- src/de/willuhn/jameica/fibu/Settings.java
+++ src/de/willuhn/jameica/fibu/Settings.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/syntax/syntax/src/de/willuhn/jameica/fibu/Settings.java,v $
- * $Revision: 1.47 $
- * $Date: 2009/07/03 10:52:19 $
+ * $Revision: 1.48 $
+ * $Date: 2010/06/01 16:35:48 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -13,8 +13,13 @@
 package de.willuhn.jameica.fibu;
 
 import java.rmi.RemoteException;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 
 import de.willuhn.datasource.rmi.ObjectNotFoundException;
+import de.willuhn.jameica.fibu.gui.util.CustomDateFormat;
+import de.willuhn.jameica.fibu.gui.util.CustomDecimalFormat;
 import de.willuhn.jameica.fibu.rmi.DBService;
 import de.willuhn.jameica.fibu.rmi.DBSupport;
 import de.willuhn.jameica.fibu.rmi.Geschaeftsjahr;
@@ -40,6 +45,36 @@ public class Settings
   public final static de.willuhn.jameica.system.Settings SETTINGS = new de.willuhn.jameica.system.Settings(Fibu.class);
 
   /**
+   * Dateformatter.
+   */
+  public final static DateFormat LONGDATEFORMAT   = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
+  /**
+   * Dateformatter.
+   */
+  public final static DateFormat DATEFORMAT       = new SimpleDateFormat("dd.MM.yyyy");
+
+  /**
+   * Dateformatter fuer Kurz-Format.
+   */
+  public final static DateFormat FASTDATEFORMAT   = new SimpleDateFormat("ddMMyyyy");
+
+  /**
+   * Dateformatter fuer Buchungen.
+   */
+  public final static DateFormat BUCHUNGDATEFORMAT   = new SimpleDateFormat("ddMMyy");
+
+  /**
+   * Unser eigenes kombiniertes Dateformat.
+   */
+  public final static DateFormat CUSTOM_DATEFORMAT   = new CustomDateFormat();
+
+  /**
+   * DecimalFormat.
+   */
+  public final static DecimalFormat DECIMALFORMAT = new CustomDecimalFormat();
+
+  /**
    * Default-Waehrung.
    */
   public final static String WAEHRUNG = SETTINGS.getString("currency.default","EUR");
@@ -49,12 +84,24 @@ public class Settings
    */
   public final static String ENCODING = SETTINGS.getString("encoding","ISO-8859-15");
 
-  private static DBService db = null;
-	private static Geschaeftsjahr jahr = null;
+
+  /**
+   * Wochentage
+   */
+  public final static String[] WEEKDAYS = new String[] {
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Sonnabend"
+  };
   
+  private static DBService db        = null;
   private static DBSupport dbSupport = null;
-  
-  private static boolean inUpdate = false;
+	private static Geschaeftsjahr jahr = null;
+  private static boolean inUpdate    = false;
   
   /**
    * Liefert true, wenn die Anwendung zum ersten Mal gestartet wird.
@@ -379,10 +426,31 @@ public class Settings
   {
     return inUpdate;
   }
+  
+  /**
+   * Liefert true, wenn das Aendern des Systemkontenrahmens erlaubt ist.
+   * @return true, wenn das Aendern des Systemkontenrahmens erlaubt ist.
+   */
+  public static boolean getSystemDataWritable()
+  {
+    return SETTINGS.getBoolean("systemdata.writable",false);
+  }
+  
+  /**
+   * Legt fest, ob das Aendern des Systemkontenrahmens erlaubt ist.
+   * @param b true, wenn das Aendern des Systemkontenrahmens erlaubt ist.
+   */
+  public static void setSystemDataWritable(boolean b)
+  {
+    SETTINGS.setAttribute("systemdata.writable",b);
+  }
 }
 
 /*********************************************************************
  * $Log: Settings.java,v $
+ * Revision 1.48  2010/06/01 16:35:48  willuhn
+ * @C Konstanten verschoben
+ *
  * Revision 1.47  2009/07/03 10:52:19  willuhn
  * @N Merged SYNTAX_1_3_BRANCH into HEAD
  *
@@ -396,148 +464,4 @@ public class Settings
  *
  * Revision 1.45.2.1  2008/06/25 10:06:19  willuhn
  * @C Default-GWG-Wert aktualisiert
- *
- * Revision 1.45  2006/12/27 14:42:23  willuhn
- * @N Update fuer MwSt.-Erhoehung
- *
- * Revision 1.44  2006/11/09 16:56:09  willuhn
- * @B Beruecksichtigung des Encodings beim Import der SQL-Files.
- *
- * Revision 1.43  2006/06/20 23:27:17  willuhn
- * @C Anzeige des aktuellen Geschaeftsjahres
- * @C Oeffnen/Schliessen eines Geschaeftsjahres
- *
- * Revision 1.42  2006/06/19 22:41:47  willuhn
- * *** empty log message ***
- *
- * Revision 1.41  2006/06/19 22:23:47  willuhn
- * @N Wizard
- *
- * Revision 1.40  2006/06/19 16:25:42  willuhn
- * *** empty log message ***
- *
- * Revision 1.39  2006/05/29 23:05:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.38  2006/03/27 20:26:53  willuhn
- * *** empty log message ***
- *
- * Revision 1.37  2006/01/04 16:04:33  willuhn
- * @B gj/mandant handling (insb. Loeschen)
- *
- * Revision 1.36  2006/01/03 23:58:35  willuhn
- * @N Afa- und GWG-Handling
- *
- * Revision 1.35  2006/01/03 17:55:53  willuhn
- * @N a lot more checks
- * @B NPEs
- * @N BuchungsTemplates pro Mandant/Kontenrahmen
- * @N Default-Geschaeftsjahr in init.sql verschoben
- * @N Handling von Eingabe von Altbestaenden im AV
- *
- * Revision 1.34  2006/01/03 13:48:14  willuhn
- * @N Halbjahresregel bei Abschreibungen
- *
- * Revision 1.33  2005/10/21 15:59:06  willuhn
- * @C getActiveGeschaeftsjahr cleanup
- *
- * Revision 1.32  2005/10/20 23:03:44  willuhn
- * @N network support
- *
- * Revision 1.31  2005/10/18 23:28:55  willuhn
- * @N client/server tauglichkeit
- *
- * Revision 1.30  2005/10/13 15:44:33  willuhn
- * @B bug 139
- *
- * Revision 1.29  2005/10/06 17:27:59  willuhn
- * @N KontoInput
- * @N Einstellungen
- *
- * Revision 1.28  2005/09/28 17:25:28  willuhn
- * *** empty log message ***
- *
- * Revision 1.27  2005/09/26 15:15:39  willuhn
- * *** empty log message ***
- *
- * Revision 1.26  2005/09/05 15:00:43  willuhn
- * *** empty log message ***
- *
- * Revision 1.25  2005/09/05 14:32:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.24  2005/09/05 13:14:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.23  2005/09/04 23:10:14  willuhn
- * *** empty log message ***
- *
- * Revision 1.22  2005/09/01 16:34:45  willuhn
- * *** empty log message ***
- *
- * Revision 1.21  2005/09/01 14:04:31  willuhn
- * *** empty log message ***
- *
- * Revision 1.20  2005/08/30 22:33:45  willuhn
- * @B bugfixing
- *
- * Revision 1.19  2005/08/29 17:46:14  willuhn
- * @N Jahresabschluss
- *
- * Revision 1.18  2005/08/29 14:54:28  willuhn
- * @B bugfixing
- *
- * Revision 1.17  2005/08/29 12:17:29  willuhn
- * @N Geschaeftsjahr
- *
- * Revision 1.16  2005/08/22 16:37:22  willuhn
- * @N Anfangsbestaende
- *
- * Revision 1.15  2005/08/16 23:14:36  willuhn
- * @N velocity export
- * @N context menus
- * @B bugfixes
- *
- * Revision 1.14  2005/08/15 23:38:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.13  2005/08/12 00:10:59  willuhn
- * @B bugfixing
- *
- * Revision 1.12  2005/08/10 17:48:02  willuhn
- * @C refactoring
- *
- * Revision 1.11  2005/08/09 23:53:34  willuhn
- * @N massive refactoring
- *
- * Revision 1.10  2005/08/08 22:54:16  willuhn
- * @N massive refactoring
- *
- * Revision 1.9  2005/08/08 21:35:46  willuhn
- * @N massive refactoring
- *
- * Revision 1.8  2004/02/09 13:05:13  willuhn
- * @C misc
- *
- * Revision 1.7  2004/01/28 00:37:32  willuhn
- * *** empty log message ***
- *
- * Revision 1.6  2004/01/28 00:31:34  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2004/01/25 19:44:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2004/01/03 18:07:22  willuhn
- * @N Exception logging
- *
- * Revision 1.3  2003/12/15 19:08:04  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2003/12/11 21:00:35  willuhn
- * @C refactoring
- *
- * Revision 1.1  2003/11/24 23:02:11  willuhn
- * @N added settings
- *
  **********************************************************************/
