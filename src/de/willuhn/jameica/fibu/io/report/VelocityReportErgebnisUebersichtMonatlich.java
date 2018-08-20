0a8667a6c0ed94a06a510675e5403c7c0b6606af
Olaf Willuhn <github@willuhn.de>
2018-08-20 15:39:46 +0200

    Auswertung "Ergebnis: monatliche Übersicht" fehlte in 2.8.1 (Inhalt der Klasse war beim Hinzufügen der Kommentare verloren gegangen)

--- src/de/willuhn/jameica/fibu/io/report/VelocityReportErgebnisUebersichtMonatlich.java
+++ src/de/willuhn/jameica/fibu/io/report/VelocityReportErgebnisUebersichtMonatlich.java
@@ -7,3 +7,45 @@
  * Jameica License.  Please consult the file "LICENSE" for details. 
  *
  **********************************************************************/
+
+package de.willuhn.jameica.fibu.io.report;
+
+import java.util.Date;
+
+/**
+ * Auswertung fuer monatliche Ergebnis-Uebersicht.
+ */
+public class VelocityReportErgebnisUebersichtMonatlich extends AbstractVelocityReport
+{
+
+  /**
+   * @see de.willuhn.jameica.fibu.io.report.AbstractVelocityReport#getData(de.willuhn.jameica.fibu.io.report.ReportData)
+   */
+  protected VelocityReportData getData(ReportData data) throws Exception
+  {
+    VelocityReportData export = new VelocityReportData();
+    export.setTemplate("ergebnisuebersicht.monatlich.vm");
+    return export;
+  }
+
+  /**
+   * @see de.willuhn.jameica.fibu.io.report.Report#getName()
+   */
+  public String getName()
+  {
+    return i18n.tr("Ergebnis: monatliche Übersicht");
+  }
+
+  /**
+   * @see de.willuhn.jameica.fibu.io.report.AbstractReport#createPreset()
+   */
+  public ReportData createPreset()
+  {
+    ReportData data = super.createPreset();
+    data.setNeedDatum(false);
+    data.setNeedKonto(false);
+    data.setTarget(i18n.tr("syntax-{0}-ergebnisuebersicht-monatlich.html", DATEFORMAT.format(new Date())));
+    return data;
+  }
+}
+
