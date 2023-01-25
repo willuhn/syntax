f4457526a0b7f905d967b66d7a3b8903fd6c66ac
Olaf Willuhn <github@willuhn.de>
2023-01-25 18:52:35 +0100

    Cache-Fehler

--- src/de/willuhn/jameica/fibu/server/KontoImpl.java
+++ src/de/willuhn/jameica/fibu/server/KontoImpl.java
@@ -77,7 +77,13 @@ public class KontoImpl extends AbstractUserObjectImpl implements Konto
    */
   public Kontenrahmen getKontenrahmen() throws RemoteException
   {
-    return (Kontenrahmen) getAttribute("kontenrahmen_id");
+    Integer i = (Integer) super.getAttribute("kontenrahmen_id");
+    if (i == null)
+      return null;
+   
+    Cache cache = Cache.get(Kontenrahmen.class,true);
+    return (Kontenrahmen) cache.get(i);
+
   }
 
   /**
@@ -408,7 +414,7 @@ public class KontoImpl extends AbstractUserObjectImpl implements Konto
    */
   public void setKontenrahmen(Kontenrahmen k) throws RemoteException
   {
-    setAttribute("kontenrahmen_id",k);
+    setAttribute("kontenrahmen_id",k == null || k.getID() == null ? null : new Integer(k.getID()));
   }
 
   /**
@@ -605,195 +611,3 @@ public class KontoImpl extends AbstractUserObjectImpl implements Konto
     return i == null ? 0 : i.intValue();
   }
 }
-
-/*********************************************************************
- * $Log: KontoImpl.java,v $
- * Revision 1.59  2011/07/04 13:05:18  willuhn
- * @N Delete-Check bei Buchungsvorlagen
- *
- * Revision 1.58  2011-05-12 09:10:31  willuhn
- * @R Back-Buttons entfernt
- * @C GUI-Cleanup
- *
- * Revision 1.57  2011-03-25 10:14:10  willuhn
- * @N Loeschen von Mandanten und Beruecksichtigen der zugeordneten Konten und Kontenrahmen
- * @C BUGZILLA 958
- *
- * Revision 1.56  2011-03-10 16:10:50  willuhn
- * @B Auswertung Kontoauszug erlaubt die Auswahl eines Zeitraumes innerhalb des Jahres - das muss in getNumBuchungen() auch beachtet werden
- *
- * Revision 1.55  2011-03-10 16:01:26  willuhn
- * @B Das Geschaeftsjahr wurde bei der Ermittlung der Anzahl der Buchungen gar nicht beruecksichtigt
- *
- * Revision 1.54  2010-06-01 16:37:22  willuhn
- * @C Konstanten von Fibu zu Settings verschoben
- * @N Systemkontenrahmen nach expliziter Freigabe in den Einstellungen aenderbar
- * @C Unterscheidung zwischen canChange und isUserObject in UserObject
- * @C Code-Cleanup
- * @R alte CVS-Logs entfernt
- *
- * Revision 1.53  2009/07/03 10:52:19  willuhn
- * @N Merged SYNTAX_1_3_BRANCH into HEAD
- *
- * Revision 1.51.2.1  2009/06/23 11:04:49  willuhn
- * @N auch nach Belegnummer sortieren
- *
- * Revision 1.51  2007/11/05 01:05:43  willuhn
- * @C refactoring
- *
- * Revision 1.50  2007/02/27 15:46:17  willuhn
- * @N Anzeige des vorherigen Kontostandes im Kontoauszug
- *
- * Revision 1.49  2006/09/05 20:57:51  willuhn
- * @B wrong import
- *
- * Revision 1.48  2006/09/05 20:57:27  willuhn
- * @ResultsetIterator merged into datasource lib
- *
- * Revision 1.47  2006/05/30 23:22:55  willuhn
- * @C Redsign beim Laden der Buchungen. Jahresabschluss nun korrekt
- *
- * Revision 1.46  2006/05/29 23:05:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.45  2006/05/29 17:30:26  willuhn
- * @N a lot of debugging
- *
- * Revision 1.44  2006/05/08 22:44:18  willuhn
- * @N Debugging
- *
- * Revision 1.43  2006/03/27 20:26:53  willuhn
- * *** empty log message ***
- *
- * Revision 1.42  2006/03/17 16:23:28  willuhn
- * *** empty log message ***
- *
- * Revision 1.41  2006/01/09 01:17:12  willuhn
- * *** empty log message ***
- *
- * Revision 1.40  2006/01/06 00:05:51  willuhn
- * @N MySQL Support
- *
- * Revision 1.39  2006/01/02 15:18:29  willuhn
- * @N Buchungs-Vorlagen
- *
- * Revision 1.38  2006/01/02 01:54:07  willuhn
- * @N Benutzerdefinierte Konten
- *
- * Revision 1.37  2005/10/18 23:28:55  willuhn
- * @N client/server tauglichkeit
- *
- * Revision 1.36  2005/10/18 09:25:31  willuhn
- * *** empty log message ***
- *
- * Revision 1.35  2005/10/06 16:00:37  willuhn
- * @B bug 135
- *
- * Revision 1.34  2005/10/04 23:36:13  willuhn
- * *** empty log message ***
- *
- * Revision 1.33  2005/10/03 14:22:11  willuhn
- * *** empty log message ***
- *
- * Revision 1.32  2005/09/27 17:41:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.31  2005/09/26 23:51:59  willuhn
- * *** empty log message ***
- *
- * Revision 1.30  2005/09/02 17:35:07  willuhn
- * @N Kontotyp
- * @N Betriebsergebnis
- *
- * Revision 1.29  2005/09/01 21:08:41  willuhn
- * *** empty log message ***
- *
- * Revision 1.28  2005/08/30 23:15:32  willuhn
- * *** empty log message ***
- *
- * Revision 1.27  2005/08/30 22:51:31  willuhn
- * @B bugfixing
- *
- * Revision 1.26  2005/08/29 12:17:29  willuhn
- * @N Geschaeftsjahr
- *
- * Revision 1.25  2005/08/25 23:00:02  willuhn
- * *** empty log message ***
- *
- * Revision 1.24  2005/08/22 23:13:26  willuhn
- * *** empty log message ***
- *
- * Revision 1.23  2005/08/22 21:44:09  willuhn
- * @N Anfangsbestaende
- *
- * Revision 1.22  2005/08/22 16:37:22  willuhn
- * @N Anfangsbestaende
- *
- * Revision 1.2  2005/08/15 23:38:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2005/08/12 00:10:59  willuhn
- * @B bugfixing
- *
- * Revision 1.20  2005/08/10 17:48:02  willuhn
- * @C refactoring
- *
- * Revision 1.19  2005/08/08 21:35:46  willuhn
- * @N massive refactoring
- *
- * Revision 1.18  2004/02/09 13:05:13  willuhn
- * @C misc
- *
- * Revision 1.17  2004/01/29 01:05:09  willuhn
- * *** empty log message ***
- *
- * Revision 1.16  2004/01/29 00:19:53  willuhn
- * *** empty log message ***
- *
- * Revision 1.15  2004/01/29 00:13:19  willuhn
- * *** empty log message ***
- *
- * Revision 1.14  2004/01/27 22:47:30  willuhn
- * *** empty log message ***
- *
- * Revision 1.13  2004/01/25 19:44:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.12  2004/01/03 18:07:22  willuhn
- * @N Exception logging
- *
- * Revision 1.11  2003/12/19 19:45:06  willuhn
- * *** empty log message ***
- *
- * Revision 1.10  2003/12/12 21:11:26  willuhn
- * @N ObjectMetaCache
- *
- * Revision 1.9  2003/12/11 21:00:34  willuhn
- * @C refactoring
- *
- * Revision 1.8  2003/12/05 17:11:58  willuhn
- * @N added GeldKonto, Kontoart
- *
- * Revision 1.7  2003/12/01 20:29:00  willuhn
- * @B filter in DBIteratorImpl
- * @N InputFelder generalisiert
- *
- * Revision 1.6  2003/11/30 16:23:11  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2003/11/27 00:21:05  willuhn
- * @N Checks via insertCheck(), deleteCheck() updateCheck() in Business-Logik verlagert
- *
- * Revision 1.4  2003/11/24 16:26:15  willuhn
- * @N AbstractDBObject is now able to resolve foreign keys
- *
- * Revision 1.3  2003/11/24 15:18:21  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2003/11/22 20:43:07  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2003/11/20 03:48:44  willuhn
- * @N first dialogues
- *
- **********************************************************************/
