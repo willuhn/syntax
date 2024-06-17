936fba6df94ab14e99f7d0a0d6978b03598b3cc5
Olaf Willuhn <github@willuhn.de>
2024-06-17 10:28:45 +0200

    Noch zu PR #6 - Name der Funktion hatte sich geändert

--- src/de/willuhn/jameica/fibu/server/BuchungImpl.java
+++ src/de/willuhn/jameica/fibu/server/BuchungImpl.java
@@ -101,8 +101,9 @@ public class BuchungImpl extends AbstractBaseBuchungImpl implements Buchung, Cus
       HilfsBuchung[] hbs = engine.buche(this);
       
       Buchung hauptbuchung = getSplitHauptBuchung();
-      //Wenn vorhanden, die SplitHauptbuchung speichern um die Steuer/Hilfsbuchungen zu entfernen
-      if(hauptbuchung != null && hauptbuchung.getSteuer() >= 0.01d) {
+      // Wenn vorhanden, die SplitHauptbuchung speichern um die Steuer/Hilfsbuchungen zu entfernen
+      if(hauptbuchung != null && hauptbuchung.getSteuerSatz() >= 0.01d)
+      {
           hauptbuchung.setBetrag(hauptbuchung.getBruttoBetrag());
           hauptbuchung.setSteuer(null);
           hauptbuchung.store();
