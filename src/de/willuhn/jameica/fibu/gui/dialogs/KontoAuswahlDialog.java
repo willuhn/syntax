e675c9351d6085566b048e92495b0f01dc78d21e
Olaf Willuhn <github@willuhn.de>
2026-04-27 15:54:55 +0200

    Größe des Kontoauswahldialogs speichern und Liste der Konten nicht mehr in der selben Breite anzeigen wie die allgemeine Kontoliste

--- src/de/willuhn/jameica/fibu/gui/dialogs/KontoAuswahlDialog.java
+++ src/de/willuhn/jameica/fibu/gui/dialogs/KontoAuswahlDialog.java
@@ -9,8 +9,13 @@
  **********************************************************************/
 package de.willuhn.jameica.fibu.gui.dialogs;
 
-import org.eclipse.swt.SWT;
+import java.rmi.RemoteException;
+
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
 
 import de.willuhn.datasource.rmi.DBIterator;
 import de.willuhn.jameica.fibu.Fibu;
@@ -22,6 +27,8 @@ import de.willuhn.jameica.gui.parts.table.FeatureSummary;
 import de.willuhn.jameica.gui.util.ButtonArea;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.jameica.system.OperationCanceledException;
+import de.willuhn.jameica.system.Settings;
+import de.willuhn.logging.Logger;
 import de.willuhn.util.ApplicationException;
 import de.willuhn.util.I18N;
 
@@ -31,6 +38,11 @@ import de.willuhn.util.I18N;
 public class KontoAuswahlDialog extends AbstractDialog
 {
 	private final static I18N i18n = Application.getPluginLoader().getPlugin(Fibu.class).getResources().getI18N();
+  private final static Settings settings = new Settings(KontoAuswahlDialog.class);
+  
+  private final static int WINDOW_WIDTH = 650;
+  private final static int WINDOW_HEIGHT = 350;
+
 	private Konto choosen = null;
   private DBIterator konten = null;
   private String query = null;
@@ -47,7 +59,7 @@ public class KontoAuswahlDialog extends AbstractDialog
     this.konten = konten;
     this.query = query;
 		this.setTitle(i18n.tr("Konto-Auswahl"));
-    this.setSize(SWT.DEFAULT,350);
+    setSize(settings.getInt("window.width",WINDOW_WIDTH),settings.getInt("window.height",WINDOW_HEIGHT));
   }
 
   /**
@@ -55,17 +67,8 @@ public class KontoAuswahlDialog extends AbstractDialog
    */
   protected void paint(Composite parent) throws Exception
   {
-    Action a = new Action() {
-      public void handleAction(Object context) throws ApplicationException
-      {
-        if (context == null || !(context instanceof Konto))
-          return;
-        choosen = (Konto) context;
-        close();
-      }
-    };
     this.konten.begin();
-		final KontoList konten = new KontoList(null,this.konten,this.query,a);
+		final KontoAuswahlList konten = new KontoAuswahlList();
     konten.setContextMenu(null);
     konten.setMulti(false);
     konten.removeFeature(FeatureSummary.class);
@@ -83,14 +86,33 @@ public class KontoAuswahlDialog extends AbstractDialog
         choosen = (Konto) o;
         close();
       }
-    },null,true);
+    },null,true,"ok.png");
 		b.addButton(i18n.tr("Abbrechen"), new Action()
     {
       public void handleAction(Object context) throws ApplicationException
       {
 				throw new OperationCanceledException();
       }
+    },null,false,"process-stop.png");
+		
+    getShell().setMinimumSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+    
+    getShell().addDisposeListener(new DisposeListener() {
+      
+      @Override
+      public void widgetDisposed(DisposeEvent e)
+      {
+        Shell shell = getShell();
+        if (shell == null || shell.isDisposed())
+          return;
+        
+        Point size = shell.getSize();
+        Logger.debug("saving window size: " + size.x + "x" + size.y);
+        settings.setAttribute("window.width",size.x);
+        settings.setAttribute("window.height",size.y);
+      }
     });
+
   }
 
   /**
@@ -101,6 +123,25 @@ public class KontoAuswahlDialog extends AbstractDialog
   protected Object getData() throws Exception
   {
     return choosen;
+  }
+  
+  /**
+   * Überschrieben, damit die Tabelle eine eigene ID bekommt und damit auch eigene Spaltenbreiten haben kann.
+   */
+  private class KontoAuswahlList extends KontoList
+  {
+    private KontoAuswahlList() throws RemoteException
+    {
+      super(null,konten,query,context -> {
+        if (context == null || !(context instanceof Konto))
+          return;
+        choosen = (Konto) context;
+        close();
+      });
+      
+      setRememberColWidths(false);
+      setRememberState(false);
+    }
   }
 
 }
